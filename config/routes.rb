b7fd7a4c2bd8e1f9a6afe1e6cad4ce1f6458d592
KaguraGateway <32214018+KaguraGateway@users.noreply.github.com>
2022-08-24 23:24:07 +0900

    [devise] ルーティングをカスタマイズした

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  devise_scope :user do
+    get '/login', to: 'devise/sessions#new'
+    post '/login', to: 'devise/sessions#create'
+    delete '/logout', to: 'devise/sessions#destroy'
+    get '/register', to: 'devise/registrations#new'
+  end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
