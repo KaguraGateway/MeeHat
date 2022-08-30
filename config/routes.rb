fa15aa37fe8c93f0642606d333c69091eb468843
KaguraGateway <32214018+KaguraGateway@users.noreply.github.com>
2022-08-30 13:48:39 +0900

    ログイン後/appに飛ぶように

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get 'app(/*path1(/*path2(/*path3(/*path4(/*path5(/*path6))))))', to: 'app#index'
-  devise_for :users, path: "", path_names: { sign_in: "login", sign_out: "logout", registration: "register" }
+  devise_for :users, path: "", path_names: { sign_in: "login", sign_out: "logout", registration: "register" }, controllers: {
+    registrations: "users/registrations"
+  }
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
